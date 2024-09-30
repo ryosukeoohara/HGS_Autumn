1302924465af7073393f140147b1b9cffe8d108f
ShionSatone <221081@st.yoshida-g.ac.jp>
2024-09-30 16:22:50 +0900

    ギミック置き場完成

--- HGS_Autumn/code/gimmick.cpp
+++ HGS_Autumn/code/gimmick.cpp
@@ -10,16 +10,27 @@
 #include "player.h"
 
 float CGimmick::m_fDestPos = 0.0f;		// 目標地点
+CGimmickTiming* CGimmick::m_pGimmickTiming = nullptr;
 
 //===========================================================
 // 定数定義
 //===========================================================
 namespace
 {
-	// ギミック開始位置
-	const float fDestPos[2] = 
-	{ -5000.0f,		// 歩行
-		-10000.0f };	// 綱渡り
+	const int NUM_GIMMICK = 15;		// ギミック数
+
+	// ギミック開始位置 ※終了から次のギミックまでは固定の値とする
+	const float fDestPos[NUM_GIMMICK] = {
+		-5000.0f,		// 綱
+		-10000.0f,		// 終了
+		-11000.0f,		// 歩行
+		-16000.0f,		// 終了
+		-17000.0f,		// 岩
+		-23000.0f,		// 終了
+		-24000.0f,		// 歩行
+		-26000.0f,		// 終了
+	
+	};
 }
 
 //===========================================================
@@ -27,10 +38,11 @@ namespace
 //===========================================================
 CGimmick::CGimmick(int nPriority)
 {
+	m_pGimmickTiming = nullptr;
 	m_GimmickType = TYPEWALK;
 	m_fDestPos = 0.0f;
 	m_bStart = false;		// ギミック開始したか
-
+	m_bEnd = false;
 }
 
 //===========================================================
@@ -57,7 +69,11 @@ HRESULT CGimmick::Init(void)
 //===========================================================
 void CGimmick::Uninit(void)
 {
-
+	if (m_pGimmickTiming != nullptr)
+	{
+		m_pGimmickTiming->Uninit();
+		m_pGimmickTiming = nullptr;
+	}
 }
 
 //===========================================================
@@ -67,27 +83,34 @@ void CGimmick::Update(void)
 {
 	CPlayer* pPlayer = CPlayer::GetInstance();
 
-	if (fDestPos[0] >= pPlayer->GetPosition().z)
+	if (fDestPos[2] >= pPlayer->GetPosition().z &&
+		fDestPos[3] <= pPlayer->GetPosition().z)
+	{ // 歩行
+
+		if (m_bStart == false)
+		{
+			m_GimmickType = TYPEWALK;
+			m_bStart = true;
+			Set(m_GimmickType);
+		}
+	}
+	else if (fDestPos[1] >= pPlayer->GetPosition().z &&
+		fDestPos[2] <= pPlayer->GetPosition().z)
+	{ // 終了
+
+		m_bStart = false;
+	}
+	else if (fDestPos[0] >= pPlayer->GetPosition().z &&
+		fDestPos[1] <= pPlayer->GetPosition().z)
 	{ // 綱渡り
 
 		if (m_bStart == false)
 		{
 			m_GimmickType = TYPEROPE;
 			m_bStart = true;
-
-		}
-	}
-
-	if (m_bStart == true)
-	{
-		switch (m_GimmickType)
-		{
-		case CGimmick::TYPEWALK:		// 歩行
-			CGimmickTiming::Create();
-			break;
-
-		default:
-			break;
+			m_pGimmickTiming->Release();
+			m_pGimmickTiming = nullptr;
+			Set(m_GimmickType);
 		}
 	}
 }
@@ -116,7 +139,27 @@ CGimmick* CGimmick::Create(float fDestDistance, TYPE type)
 	}
 
 	// 歩行
-	CGimmickTiming::Create();
+	m_pGimmickTiming = CGimmickTiming::Create();
 
 	return pGimmick;
+}
+
+//===========================================================
+// 生成処理
+//===========================================================
+void CGimmick::Set(TYPE GimmickType)
+{
+	switch (GimmickType)
+	{
+	case CGimmick::TYPEWALK:
+
+		m_pGimmickTiming = CGimmickTiming::Create();
+
+		break;
+	case CGimmick::TYPEROPE:
+		break;
+	
+	default:
+		break;
+	}
 }
