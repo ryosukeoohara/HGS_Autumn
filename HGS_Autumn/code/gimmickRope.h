5c93c4efbbbfc6820d4b9df0a00697b4da06a6d3
ShionSatone <221081@st.yoshida-g.ac.jp>
2024-09-30 16:28:38 +0900

    綱渡り.cpp追加

---
+++ HGS_Autumn/code/gimmickRope.h
@@ -0,0 +1,46 @@
+//===========================================================
+//
+// 綱渡り処理[gimmcik.h]
+// Author : 大原　怜将
+//
+//===========================================================
+#ifndef _GIMMICKROPE_H_             //このマクロ定義がされてなかったら
+#define _GIMMICKROPE_H_             //2重インクルード防止のマクロ定義をする
+
+#include "gimmick.h"
+
+class CGimmick;
+class CPlayer;
+
+//===========================================================
+// 定数定義
+//===========================================================
+namespace
+{
+	const int NUM_JUDGE = 2;           // 判定の数
+}
+
+//===========================================================
+// ギミッククラス定義
+//===========================================================
+class CGimmickRope : public CGimmick
+{
+public:
+
+	CGimmickRope(int nPriority = 3);  // コンストラクタ
+	~CGimmickRope();                  // デストラクタ
+
+	HRESULT Init(void);            // 初期化処理    
+	void Uninit(void);             // 終了処理
+	void Update(void);             // 更新処理
+	void Draw(void);               // 描画処理
+
+	static CGimmickRope* Create(void);        // 生成処理
+
+private:
+	static CBillBoard* m_pBillBoard[NUM_JUDGE];		// ビルボードの情報
+	float m_fMove;		// 移動量
+
+};
+
+#endif
